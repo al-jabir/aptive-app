b6a1fd94558d73f5b14866047a8653ac53e25794
al-jabir <aljabir098@gmail.com>
2022-10-15 20:58:56 +0600

    I have done and responsive

---
+++ src/components/FAQ/FAQ.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './faq.css';
+
+const FAQ = () => {
+  return (
+    <div className='section-faq'>
+      <div className='container'>
+        <div className='col-1'>
+          <h3>Frequently asked questions</h3>
+          <p>
+            Can't out to our <span>customer support</span>team.
+          </p>
+        </div>
+        <div className='col-2'>
+          <div className='faq'>
+            <h5>How do you make water</h5>
+            <p>You biol the hell out of it. Lorem ipsum olor site amet.</p>
+          </div>
+          <div className='faq'>
+            <h5>How do you make water</h5>
+            <p>You biol the hell out of it. Lorem ipsum olor site amet.</p>
+          </div>
+          <div className='faq'>
+            <h5>How do you make water</h5>
+            <p>You biol the hell out of it. Lorem ipsum olor site amet.</p>
+          </div>
+          <div className='faq'>
+            <h5>How do you make water</h5>
+            <p>You biol the hell out of it. Lorem ipsum olor site amet.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FAQ;
